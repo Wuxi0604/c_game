1915fc2f38f7d261416f0b6e4e7d614f6a17b3f4
wx <a1363182250@163.com>
2022-11-08 22:39:50 +0800

    添加功能 简单弹跳小球

---
+++ c_game/c_game.h
@@ -0,0 +1,13 @@
+#ifndef C_GAME_H
+#define C_GAME_H
+
+typedef signed char         sint8;
+typedef unsigned char       uint8;
+typedef signed int          sint16;
+typedef unsigned int        uint16;
+typedef signed long         sint32;
+typedef unsigned int        uint32;
+typedef signed long long    sint64;
+typedef unsigned long long  uint64;
+
+#endif
---
+++ c_game/simple_boll.c
@@ -0,0 +1,83 @@
+#include "c_game.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <assert.h>
+
+
+
+void
+print_border(uint16 length, uint16 width, uint16 boll_x, uint16 boll_y, char border_type)
+{
+    assert(length <= 60);
+    assert(width <= 20);
+    uint16 x;
+    uint16 y;
+
+    for(x = 0; x < width; x++)
+    {
+        //边界上不能有球
+        if(x == 0 || x == width - 1)
+        {
+            for(y = 0; y < length; y++)
+                printf("%c", border_type);
+            printf("\n");
+            continue;
+        }
+        for(y = 0; y < length; y++)
+        {
+            if( y == 0 || y == length-1)
+                printf("%c", border_type);
+            else if(boll_x == x && boll_y == y)
+            {
+                printf("o");
+                Sleep(50);
+            }else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int
+main()
+{
+    //sint16 i, j;
+    sint16 x = 5;
+    sint16 y = 5;
+
+    //定义小球的边框
+    sint16 top = 0;
+    sint16 bottom = 20;
+    sint16 left = 0;
+    sint16 right = 60;
+
+    sint16 velocity_x = 1;
+    sint16 velocity_y = 1;
+
+    //每一次循环的时候都需要重新绘制边框
+    while(1 == 1)
+    {
+        x = x + velocity_x;
+        y = y + velocity_y;
+        system("cls");
+        print_border(right - left, bottom - top, x, y, '-');
+        //for(i = 0; i < y; i++)
+        //    printf("\n");
+
+        //for(j = 0; j < x; j++)
+        //    printf(" ");
+
+        //printf("o");
+        //printf("\n");
+        //Sleep(50);
+
+        if((y == top) || (y == bottom))
+            velocity_y = - velocity_y;
+
+        if((x == left) || (x == right))
+            velocity_x = - velocity_x;
+    }
+}
